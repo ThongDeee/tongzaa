30059519422064f49198f3185103731ba1e4f491
ThongDeee <68993830+ThongDeee@users.noreply.github.com>
2021-01-29 23:34:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 
 def handle_message(event):
-    
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text="alohamora"))
+    Awada=message
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f"{Awada}"))
 
 
 
