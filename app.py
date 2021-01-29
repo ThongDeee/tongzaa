dd3c67c9e17959ee262208f3623c8b441a152406
ThongDeee <68993830+ThongDeee@users.noreply.github.com>
2021-01-30 00:07:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,17 @@ def callback():
 
 def handle_message(event):
     Awada = (event.message.text)
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f"{Awada}"))
+    def getbotnoi(Awada):
+        url = f"https://openapi.botnoi.ai/botnoi/ecommerce?keyword={Awada}" 
+        headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTIwOTM5MjcsImlkIjoiNTJjYTJjODgtNDI2Ny00ZDEwLTkwNzktYmE4MGMxZWNhMzQ5IiwiaXNzIjoiZnloRHFJV1Npb3R4YmN3MkI4amZ5dUJBdUNHdFRLcm4iLCJuYW1lIjoiMTQzMy4wOCIsInBpYyI6Imh0dHBzOi8vcHJvZmlsZS5saW5lLXNjZG4ubmV0LzBoR1diUjVwYTVHSGhmS3pCcDdReG5MMk51RmhVb0JSNHdKMGxXR0h3dlFFMTJIVjRvWVJoVlNuOV9RMHh4SFZncE1VNEhHM3A1UWgxdyJ9.wfEGlqTBL1YQMsKWOEunptFk3mudSINF0ohdjTraCD0'
+}
+        response = requests.request("Get", url, headers=headers).json()
+        if response['intent'] ==  "ขอเวลาปิดทำการ":
+            return "ไม่มีวันปิดครับ"
+        else: 
+            return "ไม่เข้าใจคำถาม"
+    Bunny = getbotnoi(Awada)
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f"{Bunny}"))
 
 
 
