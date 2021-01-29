49f6e736d3fd1488a8dfca842453a3a6d82de69b
ThongDeee <68993830+ThongDeee@users.noreply.github.com>
2021-01-29 23:21:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 
 def handle_message(event):
-    Saha=message
+    
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
 
