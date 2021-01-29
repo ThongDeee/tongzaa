c9d1e81d35286720d9aa77535ab6647c5ec3e010
ThongDeee <68993830+ThongDeee@users.noreply.github.com>
2021-01-29 20:03:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,9 +52,7 @@ def handle_message(event):
             return "ไม่เข้าใจคำถาม"
 
     aimessage=getbotnoi(message)
-    line_bot_api.reply_message(
-        event.reply_token,
-        aimessage)
+    line_bot_api.reply_message(event.reply_token,aimessage)
 
 
 
