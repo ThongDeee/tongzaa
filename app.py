aa78375b47a7ce220c156a342a0f0dd60afda12f
ThongDeee <68993830+ThongDeee@users.noreply.github.com>
2021-02-03 05:39:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ class get_botnoi(Resource):
             url = f"https://openapi.botnoi.ai/service-api/botnoichitchat?key={}&styleid=6&botname=บอท" 
             headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTI1NTAxMzgsImlkIjoiNTJjYTJjODgtNDI2Ny00ZDEwLTkwNzktYmE4MGMxZWNhMzQ5IiwiaXNzIjoiZnloRHFJV1Npb3R4YmN3MkI4amZ5dUJBdUNHdFRLcm4iLCJuYW1lIjoiMTQzMy4wOCIsInBpYyI6Imh0dHBzOi8vcHJvZmlsZS5saW5lLXNjZG4ubmV0LzBoR1diUjVwYTVHSGhmS3pCcDdReG5MMk51RmhVb0JSNHdKMGxXR0h3dlFFMTJIVjRvWVJoVlNuOV9RMHh4SFZncE1VNEhHM3A1UWgxdyJ9.4OfNBZoVPN4cE4TJr5ybBzeRto0pz045lUcDB5iCBA4'}
             response = requests.request("Get", url, headers=headers).json()        
-                return 'reply'
+                return 'response'
         ans=get(self)
         result={}
-        result['reply']=ans
+        
         return result
 
 api.add_resource(get_botnoi, '/cal',endpoint='cal')
