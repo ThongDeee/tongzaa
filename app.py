fc987c0c9843f1626e67c2d258381c231738ee81
ThongDeee <68993830+ThongDeee@users.noreply.github.com>
2021-02-04 02:59:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ class WeatherCity(Resource):
 api.add_resource(WeatherCity,"/w/<string:name>")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
